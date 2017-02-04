3fa1af76059a2228ea9451b926c66cbce09bea68
Richard Du <richarddu@Richards-Mac.local>
2017-02-03 18:30:32 -0800

    First commit

---
+++ Pods/Target Support Files/Pods-TwitterDemo-TwitterDemoTests/Pods-TwitterDemo-TwitterDemoTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TwitterDemo_TwitterDemoTests : NSObject
+@end
+@implementation PodsDummy_Pods_TwitterDemo_TwitterDemoTests
+@end
---
+++ Pods/Target Support Files/Pods-TwitterDemo-TwitterDemoUITests/Pods-TwitterDemo-TwitterDemoUITests-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_TwitterDemo_TwitterDemoUITestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_TwitterDemo_TwitterDemoUITestsVersionString[];
+
